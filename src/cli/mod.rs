8a3686f827b096d1a9231da5e0923fae0a1cf827
experiment314159 <yousef.hisham444@proton.me>
2024-11-24 22:05:13 +0200

    .

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,11 +3,6 @@ use crate::{cli_build_interface, subcommands_build_interface};
 use clap::{Args, Parser, Subcommand, ValueEnum};
 use std::path::PathBuf;
 
-#[derive(Parser)]
-#[command(author, version, about)]
-pub struct Cli {
-    #[command(subcommand)]
-    pub command: GystSubcommand,
-}
+cli_build_interface!(Cli { subcommands GystSubcommand, });
 
 subcommands_build_interface!(GystSubcommand {});
