a65f18515dd7c1ba0144aaa076363b0c3b5acfd4
experiment314159 <yousef.hisham444@proton.me>
2024-12-10 22:18:15 +0200

    first build of  the cli macro

--- src/cli/cli_build.rs
+++ src/cli/cli_build.rs
@@ -1,33 +1,44 @@
-pub struct SubcommandInfo {
-    pub name: String,
-    pub about: String,
-    pub long_about: String,
-    pub args: Vec<String>,
-    pub options: Vec<String>
+use paste::paste;
+
+#[macro_export]
+macro_rules! pub_struct {
+    (
+        $(#[$struct_attr:meta])*
+        struct $struct:ident {
+            $($(#[$field_attr:meta])* $field:ident: $t:ty),*
+        }
+    ) => {
+        $(#[$struct_attr])*
+        pub struct $struct {
+            $($(#[$field_attr])* pub $field: $t),*
+        }
+    }
 }
+
+pub_struct!(
+    struct SubcommandInfo {
+        name: String,
+        about: String,
+        long_about: String,
+        args: Vec<String>,
+        options: Vec<String>
+    }
+);
 
 #[macro_export]
 macro_rules! subcommand_args {
     (
-        $($subcommand_args_attr:meta),* $subcommand_args:ident {
-            name => $subcommand:ident,
+        $($subcommand_args_attr:meta),* $subcommand:ident {
             about => $about:expr,
-            long_about => $long_about:expr,
-            fn_info => $fn_info:ident
+            long_about => $long_about:expr
             $(,
-                args {
-                    $($($arg_attr:meta),* $arg:ident : $arg_type:ty => $arg_des:expr),*
-                }
+                args { $($($arg_attr:meta),* $arg:ident : $arg_type:ty => $arg_des:expr),* }
             )?
             $(,
-                options {
-                    $($($option_attr:meta),* $option:ident : $option_type:ty => $option_des:expr),*
-                }
+                options { $($($option_attr:meta),* $option:ident : $option_type:ty => $option_des:expr),* }
             )?
             $(,
-                enums {
-                    $($($enum_attr:meta),* $enum_name:ident { $($enum:ident => $enum_des:expr),* }),*
-                }
+                enums { $($($enum_attr:meta),* $enum_name:ident { $($enum:ident => $enum_des:expr),* }),* }
             )?
         }
     ) => {
@@ -36,27 +47,97 @@ macro_rules! subcommand_args {
                 $(#[$enum_attr])*
                 #[derive(Clone, ValueEnum)]
                 pub enum $enum_name {
-                    $(
-                        #[doc = $enum_des]
-                        $enum
-                    ),*
+                    $(#[doc = $enum_des] $enum),*
                 }
             )*
         )?
 
-        pub fn $fn_info() -> SubcommandInfo {
-            SubcommandInfo {
-                name: String::from(stringify!($subcommand)),
-                about: String::from($about),
-                long_about: String::from($long_about),
-                args: vec![$($(String::from(stringify!($arg))),*)?],
-                options: vec![$($(String::from(stringify!($option))),*)?]
+        paste! {
+            pub fn [<$subcommand _info()>] -> SubcommandInfo {
+                SubcommandInfo {
+                    name: String::from(stringify!($subcommand)),
+                    about: $about.to_string(),
+                    long_about: $long_about.to_string(),
+                    args: vec![$( $(stringify!($arg).to_string()),* )?],
+                    options: vec![$( $(stringify!($option).to_string()),* )?]
+                }
+            }
+
+            $(#[$subcommand_arg_attr:meta])*
+            #[derive(Args)]
+            pub struct [<$subcommand _args()>] {
+                $(
+                    $(
+                        $(#[$arg_attr])*
+                        #[arg(help = $arg_des, required = true)]
+                        pub $arg: $arg_type,
+                    )*
+                )?
+                $(
+                    $(
+                        $(#[$option_attr])*
+                        #[arg(help = $option_des, required = false)]
+                        pub $option: $option_type,
+                    )*
+                )?
             }
         }
+    };
+}
 
-        $(#[$subcommand_arg_attr:meta])*
-        #[derive(Args)]
-        pub struct $subcommand_args {
+#[macro_export]
+macro_rules! subcommands_enum {
+    (
+        $($subcommands_enum_attr:meta),* $subcommands_enum:ident {
+            $($($subcommand_attr:meta),* $subcommand:ident),*
+        }
+    ) => {
+        paste! {
+            $(#[$subcommands_enum_attr]),*
+            #[derive(Subcommand)]
+            pub enum $subcommands_enum {
+                $(
+                    $(#[$subcommand_attr]),*
+                    #[command(about = [<$subcommand _info>].about, long_about = [<$subcommand _info>].long_about)]
+                    $subcommand([<$subcommand _args>])
+                ),*
+            }
+        }
+    };
+}
+
+
+#[macro_export]
+macro_rules! cli {
+    (
+        $($cli_attr:meta),* $cli:ident {
+            $(
+                subcommands : $subcommands_enum:ident 
+            )?
+            $(,
+                args { $($($arg_attr:meta),* $arg:ident : $arg_type:ty => $arg_des:expr),* }
+            )?
+            $(,
+                options { $($($option_attr:meta),* $option:ident : $option_type:ty => $option_des:expr),* }
+            )?
+            $(,
+                enums { $($($enum_attr:meta),* $enum_name:ident { $($enum:ident => $enum_des:expr),* }),* }
+            )?
+        }
+    ) => {
+        $(
+            $(
+                $(#[$enum_attr])*
+                #[derive(Clone, ValueEnum)]
+                pub enum $enum_name {
+                    $(#[doc = $enum_des] $enum),*
+                }
+            )*
+        )?
+
+        #[derive(Parser)]
+        struct $cli {
+            $( #[command(subcommand)] command: $subcommands_enum, )?
             $(
                 $(
                     $(#[$arg_attr])*
@@ -72,34 +153,5 @@ macro_rules! subcommand_args {
                 )*
             )?
         }
-    };
-}
-
-#[macro_export]
-macro_rules! subcommands_enum {
-    (
-        $($subcommands_enum_attr:meta),* $subcommands_enum:ident {
-            $($($subcommand_attr:meta),* $subcommand:ident($subcommand_args:ident) => $subcommand_info:ident),*
-        }
-    ) => {
-        $(#[$subcommands_enum_attr]),*
-        #[derive(Subcommand)]
-        pub enum $subcommands_enum {
-            $(
-                $(#[$subcommand_attr]),*
-                #[command(about = $subcommand_info.about, long_about = $subcommand_info.long_about)]
-                $subcommand($subcommand_args)
-            ),*
-        }
-    };
-}
-
-
-#[macro_export]
-macro_rules! cli_args {
-    (
-
-    ) => {
-
     };
 }
