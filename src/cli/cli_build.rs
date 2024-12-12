dbb816a6ba50b81024eddd71dac14bb3aadfb514
experiment314159 <yousef.hisham444@proton.me>
2024-12-12 20:59:46 +0200

    1. new architecture 2nd build 2. testing still needed

--- src/cli/cli_build.rs
+++ src/cli/cli_build.rs
@@ -1,30 +1,30 @@
 #[macro_export]
 macro_rules! subcommand {
     (
-        #[$($subcommand_args_attr:meta),*] $subcommand:ident {
-            about => $about:expr,
-            long_about => $long_about:expr
-            $(,
+        $(#[$subcommand_args_attr:meta])* $subcommand:ident {
+            about => $about:expr;
+            long_about => $long_about:expr;
+            $(
                 args { $(
-                    #[$($arg_attr:meta),*] $arg:ident : $arg_type:ty => $arg_des:expr
-                ),* }
+                    $(#[$arg_attr:meta])* $arg:ident : $arg_type:ty => $arg_des:expr
+                ),* };
             )?
-            $(,
+            $(
                 options { $(
-                    #[$($option_attr:meta),*] $option:ident : $option_type:ty => $option_des:expr
-                ),* }
+                    $(#[$option_attr:meta])* $option:ident : $option_type:ty => $option_des:expr
+                ),* };
             )?
-            $(,
+            $(
                 enums { $(
-                    #[$($enum_attr:meta),*] $enum:ident { $($variant:ident => $enum_des:expr),* }
-                ),* }
+                    $(#[$enum_attr:meta])* $enum:ident { $($variant:ident => $enum_des:expr),* }
+                ),* };
             )?
         }
     ) => {
         $(
             $(
-                $(#[$enum_attr])*
                 #[derive(Clone, ValueEnum)]
+                $(#[$enum_attr])*
                 pub enum $enum { $(
                     #[doc = $enum_des] $variant
                 ),* }
@@ -32,21 +32,21 @@ macro_rules! subcommand {
         )?
 
         paste! {            
-            $(#[$subcommand_args_attr:meta])*
             #[derive(Args)]
             #[command(about = $about, long_about = $long_about)]
+            $(#[$subcommand_args_attr:meta])*
             pub struct [<$subcommand Args>] {
                 $(
                     $(
-                        $(#[$arg_attr])*
                         #[arg(help = $arg_des, required = true)]
+                        $(#[$arg_attr])*
                         pub $arg: $arg_type,
                     )*
                 )?
                 $(
                     $(
-                        $(#[$option_attr])*
                         #[arg(help = $option_des, required = false)]
+                        $(#[$option_attr])*
                         pub $option: $option_type,
                     )*
                 )?
@@ -56,58 +56,55 @@ macro_rules! subcommand {
 }
 
 #[macro_export]
-macro_rules! subcommands_enum {
-    (
-        #[$($subcommands_enum_attr:meta),*] $subcommands_enum:ident {
-            $(#[$($subcommand_attr:meta),*] $subcommand:ident),*
-        }
-    ) => {
-        paste! {
-            $(#[$subcommands_enum_attr])*
-            #[derive(Subcommand)]
-            pub enum $subcommands_enum {
-                $(
-                    $(#[$subcommand_attr])*
-                    $subcommand([<$subcommand Args>])
-                ),*
-            }
-        }
-    };
-}
-
-
-#[macro_export]
 macro_rules! cli {
     (
-        #[$($cli_attr:meta),*] $cli:ident {
+        $(#[$cli_attr:meta])* $cli:ident {
             $(
-                subcommands : $subcommands_enum:ident 
+                $(#[$subcommands_enum_attr:meta])* $subcommands_enum:ident { $($subcommand:ident),* };
             )?
-            $(,
-                args { $($($arg_attr:meta),* $arg:ident : $arg_type:ty => $arg_des:expr),* }
+            $(
+                args { $(
+                    $(#[$arg_attr:meta])* $arg:ident : $arg_type:ty => $arg_des:expr
+                ),* };
             )?
-            $(,
-                options { $($($option_attr:meta),* $option:ident : $option_type:ty => $option_des:expr),* }
+            $(
+                options { $(
+                    $(#[$option_attr:meta])* $option:ident : $option_type:ty => $option_des:expr
+                ),* };
             )?
-            $(,
-                enums { $($($enum_attr:meta),* $enum:ident { $($variant:ident => $enum_des:expr),* }),* }
+            $(
+                enums { $(
+                    $(#[$enum_attr:meta])* $enum:ident { $($variant:ident => $enum_des:expr),* }
+                ),* };
             )?
         }
     ) => {
         $(
+            paste! {
+                #[derive(Subcommand)]
+                $(#[$subcommands_enum_attr])*
+                pub enum $subcommands_enum { $(
+                    $subcommand([<$subcommand Args>])
+                ),* }
+            }
+        )?
+
+        $(
             $(
-                $(#[$enum_attr])*
                 #[derive(Clone, ValueEnum)]
-                pub enum $enum {
-                    $(#[doc = $enum_des] $variant),*
-                }
+                $(#[$enum_attr])*
+                pub enum $enum { $(
+                    #[doc = $enum_des] $variant
+                ),* }
             )*
         )?
 
         #[derive(Parser)]
         $(#[$cli_attr])*
         pub struct $cli {
-            $( #[command(subcommand)] pub command: $subcommands_enum, )?
+            $( 
+                #[command(subcommand)] pub command: $subcommands_enum,
+            )?
             $(
                 $(
                     $(#[$arg_attr])*
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,25 +1,20 @@
 mod cli_build;
 mod styles;
-use crate::{subcommand, subcommands_enum, cli};
+use crate::{subcommand, cli};
 use crate::cli::styles::get_styles;
 use clap::{Args, Parser, Subcommand};
 use paste::paste;
 
 subcommand!(
-    #[] New {
-        about => "hi",
-        long_about => "haaaay"
-    }
-);
-
-subcommands_enum!(
-    #[] Commands {
-        #[] New
+    New {
+        about => "hi";
+        long_about => "haaaay";
     }
 );
 
 cli!(
-    #[command(styles=get_styles())] Cli {
-        subcommands : Commands
+    #[command(styles=get_styles())] 
+    Cli {
+        Commands { New };
     }
 );
