34a7c0e8ad5958cb47c90c7e59fdac1d45733269
JosephTheExperiment <yousef.hisham444@proton.me>
2025-08-14 11:19:00 +0300

    REDESIGN structs in commands module, and the progect structure 0

--- src/commands/base.rs
+++ src/commands/base.rs
@@ -1,27 +1,57 @@
 use crate::printing::base::StylizedStrings;
 use crate::pub_struct;
 
-pub enum CommandErrors {
-    ValidateInput(ErrorMassage),
-    ValidateState(ErrorMassage),
-    Runtime(ErrorMassage),
+pub enum CliErrorsTypes {
+    RequiredInputPrompting(CliErrorMassage),
+    CommandNameCorrection(CliErrorMassage),
+    ValidateInput(CliErrorMassage),
+    ValidateState(CliErrorMassage),
+    Runtime(CliErrorMassage),
     None,
 }
 
 pub_struct! {
-    struct ErrorMassage {
-        error: String,
-        massage: String,
-        suggestions: Vec<String>
+    struct CliErrorMassage {}
+}
+
+// In progress :)
+// pub trait Subcommand {
+//     fn required_input_prompting(&self, cli: &Cli);
+//     fn command_name_correction(&self, cli: &Cli);
+//     fn validate_input(&self, cli: Cli) -> Result<Cli, CommandErrors>;
+//     fn validate_state(&self, cli: Cli) -> Result<Cli, CommandErrors>;
+//     fn run(&self, cli: Cli) -> Result<Cli, CommandErrors>;
+//     fn compilation(&self, cli: Cli, errors: CommandErrors) -> Result<(), CommandErrors>;
+// }
+
+pub_struct! {
+    #[derive(Clone)]
+    struct CliData {
+        name: String,
+        verison: String,
+        description: String,
+        command_data: Vec<CommandData>,
+        read_more: Vec<ReadMore>
     }
 }
 
-// pub trait Command {
-//     fn validate_input(&self, cli: &Cli) -> Result<(), CommandErrors>;
-//     fn validate_state(&self, cli: &Cli) -> Result<(), CommandErrors>;
-//     fn run(&self, cli: &Cli) -> Result<(), CommandErrors>;
-//     fn compilation(&self, cli: Cli, errors: CommandErrors) -> Result<(), CommandErrors>;
-// }
+pub_struct! {
+    #[derive(Clone)]
+    struct ReadMore {}
+}
+
+pub_struct! {
+    #[derive(Clone)]
+    struct CommandData {
+        name: String,
+        description: String,
+        detailed_description: String,
+        examples: Vec<Example>,
+        arguments: Vec<Input>,
+        flags: Vec<Header<Input>>,
+        read_more: Vec<StylizedStrings>
+    }
+}
 
 pub_struct! {
     #[derive(Clone)]
@@ -32,26 +62,8 @@ pub_struct! {
 }
 
 pub_struct! {
-    struct CliData {
-        name: String,
-        verison: String,
-        description: StylizedStrings,
-        command_data: Vec<CommandData>,
-        read_more: Vec<StylizedStrings>
-    }
-}
-
-pub_struct! {
     #[derive(Clone)]
-    struct CommandData {
-        name: String,
-        description: StylizedStrings,
-        detailed_description: StylizedStrings,
-        examples: Vec<StylizedStrings>,
-        arguments: Vec<Input>,
-        options: Vec<Header<Input>>,
-        read_more: Vec<StylizedStrings>
-    }
+    struct Example {}
 }
 
 #[derive(Clone)]
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 pub mod commands;
 pub mod data;
-pub mod features;
 pub mod parser;
 pub mod printing;
 pub mod system;
+pub mod base;
 pub mod macros;
 
 fn main() {}
